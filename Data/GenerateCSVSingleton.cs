5e5523e429dd462f7bac706ce2f275def40f34f2
Cameron <cameronhunt88@hotmail.com>
2020-12-04 12:34:27 +0000

    Changes

---
+++ Data/GenerateCSVSingleton.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TrackTrace.Business;
+
+namespace TrackTrace.Data
+{
+    /// <summary>
+    /// Author: Cameron Hunt
+    /// Date last modified: 03/12/2020
+    /// This class provides a method for exporting the data to a csv file
+    /// It uses the Singleton design pattern.
+    /// </summary>
+    class GenerateCSVSingleton
+    {
+        // Instance of class
+        private static GenerateCSVSingleton instance;
+
+        // Private constructor - cannot use new keyword
+        private GenerateCSVSingleton() { }
+
+        public static GenerateCSVSingleton Instance
+        {
+            get
+            {
+                // Check if instance has been created already
+                if (instance == null)
+                    // If not then create new instance 
+                    instance = new GenerateCSVSingleton();
+                return instance;
+            }
+        }
+
+        public void GenerateCSV(List<User> userData, List<LocationSingleton> locationData, List<VisitSingleton> visitData, List<ContactSingleton> contactData)
+        {
+            StreamWriter writer = new StreamWriter(File.OpenWrite(@"D:\C# Coursework\TrackTrace\Test.csv"));
+            foreach (User user in userData)
+            {
+                writer.WriteLine("User Data:");
+                writer.WriteLine(user.UserId);
+                writer.WriteLine(user.PhoneNumber);
+            }
+            foreach (LocationSingleton location in locationData)
+            {
+                writer.WriteLine("");
+                writer.WriteLine("Location:");
+                writer.WriteLine(location.LocationName);
+            }
+            foreach (VisitSingleton visit in visitData)
+            {
+                writer.WriteLine("");
+                writer.WriteLine("Visit Data:");
+                writer.WriteLine(visit.UserId);
+                writer.WriteLine(visit.Date);
+                writer.WriteLine(visit.Time);
+                writer.WriteLine(visit.Location);
+            }
+            foreach (ContactSingleton contact in contactData)
+            {
+                writer.WriteLine("");
+                writer.WriteLine("Contact Data:");
+                writer.WriteLine(contact.Individual1);
+                writer.WriteLine(contact.Individual2);
+                writer.WriteLine(contact.PhoneNumber);
+                writer.WriteLine(contact.Date);
+                writer.WriteLine(contact.Time);
+            }
+            writer.Close();
+        }
+    }
+}
---
+++ Data/Serialize.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+using TrackTrace.Business;
+
+namespace TrackTrace.Data
+{
+    /// <summary>
+    /// Author: Cameron Hunt
+    /// Date last modified: 03/12/2020
+    /// This class is for serializing lists of objects. It has one method for Serialization and one method for Deserialization.
+    /// </summary>
+    [Serializable]
+    class Serialize
+    {
+        private static string directory = @"D:\C# Coursework\TrackTrace\";
+        private static string serializationFile = Path.Combine(directory + "data.xml");
+        /* EXPERIMENTAL
+        public void ExportToFile(AllData allData)
+        {
+            // Serialization
+            using (Stream stream = File.Open(serializationFile, FileMode.Create))
+            {
+                var binaryFormatter = new System.Runtime.Serialization.Formatters.Binary.BinaryFormatter();
+                binaryFormatter.Serialize(stream, allData);
+            }
+        }*/
+
+        public void ExportToFile(List<User> list)
+        {
+            // Serialization
+            using (Stream stream = File.Open(serializationFile, FileMode.Create))
+            {
+                var binaryFormatter = new System.Runtime.Serialization.Formatters.Binary.BinaryFormatter();
+                binaryFormatter.Serialize(stream, list);
+            }
+        }
+        public List<User> ImportFromFile()
+        {
+            // Deserialization
+            List<User> importedUsers = new List<User>();
+            using (Stream stream = File.Open(serializationFile, FileMode.Open))
+            {
+                var binaryFormatter = new System.Runtime.Serialization.Formatters.Binary.BinaryFormatter();
+                importedUsers = (List<User>)binaryFormatter.Deserialize(stream);
+            }
+            return importedUsers;
+        }
+    }
+}
